0622046e535c95ce0c29c8af9dcb5be8212fb6ae
Sam Chau <chaucted@gmail.com>
2025-10-18 00:09:35 +1030

    chore(setup): remove existing codebase

--- backend/app/importer/strategies/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Import strategies."""
-from .base import ImportStrategy
-from .format import FormatStrategy
-from .profile import ProfileStrategy
-
-__all__ = ['ImportStrategy', 'FormatStrategy', 'ProfileStrategy']
--- backend/app/task/backup/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/task/backup/__init__.py
-from .backup import BackupManager
-
-__all__ = ['BackupManager']
