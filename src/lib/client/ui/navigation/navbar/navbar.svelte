f2d9a8fa762fa65a1daeb34e4772663ea539e21f
Sam Chau <chaucted@gmail.com>
2025-11-03 21:10:05 +1030

    style(logo): use temp firefox logo

--- src/lib/client/ui/navigation/navbar/navbar.svelte
+++ src/lib/client/ui/navigation/navbar/navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import ThemeToggle from './themeToggle.svelte';
-	import logo from '$assets/logo.svg';
+	import logo from '$assets/logo-firefox-circular-arrow.png';
 </script>
 
 <nav
@@ -9,7 +9,7 @@
 	<div class="flex items-center justify-between px-4 py-4">
 		<!-- Left: Brand name with logo -->
 		<div class="flex items-center gap-2">
-			<img src={logo} alt="Profilarr logo" class="h-5 w-5 translate-y-[3px]" />
+			<img src={logo} alt="Profilarr logo" class="h-5 w-5 translate-y-[2px]" />
 			<div class="text-xl font-bold text-neutral-900 dark:text-neutral-100">profilarr</div>
 		</div>
 
