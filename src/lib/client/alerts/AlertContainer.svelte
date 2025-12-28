d120aa5d0201398a0c35210153612f7437ffa16e
Sam Chau <schau22@pm.me>
2025-12-29 01:35:23 +1030

    fix(alerts): adjust alert container position for improved visibility

--- src/lib/client/alerts/AlertContainer.svelte
+++ src/lib/client/alerts/AlertContainer.svelte
@@ -3,7 +3,7 @@
 	import Alert from './Alert.svelte';
 </script>
 
-<div class="pointer-events-none fixed top-20 right-4 z-50 flex flex-col gap-3">
+<div class="pointer-events-none fixed top-4 right-4 z-50 flex flex-col gap-3">
 	{#each $alertStore as alert (alert.id)}
 		<div class="pointer-events-auto">
 			<Alert id={alert.id} type={alert.type} message={alert.message} />
