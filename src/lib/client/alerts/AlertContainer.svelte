284c44d108a92ff7f1e39eeb2f8d24343e3a9669
Sam Chau <schau22@pm.me>
2026-01-29 04:09:58 +1030

    style(alerts): full width, no transparency for mobile

--- src/lib/client/alerts/AlertContainer.svelte
+++ src/lib/client/alerts/AlertContainer.svelte
@@ -3,7 +3,7 @@
 	import Alert from './Alert.svelte';
 </script>
 
-<div class="pointer-events-none fixed top-4 right-4 z-50 flex flex-col gap-3">
+<div class="pointer-events-none fixed top-4 left-4 right-4 z-50 mx-auto flex max-w-md flex-col gap-3 md:left-auto md:mx-0">
 	{#each $alertStore as alert (alert.id)}
 		<div class="pointer-events-auto">
 			<Alert id={alert.id} type={alert.type} message={alert.message} />
