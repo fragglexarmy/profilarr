05ffece92e9168c33fe33e3d39d6654bbe8ec09a
Sam Chau <schau22@pm.me>
2026-02-12 20:50:14 +1030

    style: add danger variant to label

--- src/lib/client/ui/label/Label.svelte
+++ src/lib/client/ui/label/Label.svelte
@@ -7,6 +7,7 @@
 		| 'ghost'
 		| 'success'
 		| 'warning'
+		| 'danger'
 		| 'info' = 'default';
 	export let size: 'sm' | 'md' | 'lg' = 'md';
 	export let rounded: 'sm' | 'md' | 'full' = 'full';
@@ -28,6 +29,8 @@
 			'bg-emerald-100 text-emerald-800 dark:bg-emerald-900 dark:text-emerald-200',
 		warning:
 			'bg-amber-100 text-amber-800 dark:bg-amber-900 dark:text-amber-200',
+		danger:
+			'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200',
 		info:
 			'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200'
 	};
