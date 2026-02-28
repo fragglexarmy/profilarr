c19a40781b50e1ab2a99781402502a049bafe0de
Sam Chau <schau22@pm.me>
2026-02-28 17:41:05 +1030

    feat(nav): go to library page if sync is already set

--- src/routes/arr/[id]/+page.server.ts
+++ src/routes/arr/[id]/+page.server.ts
@@ -1,7 +1,17 @@
 import { redirect } from '@sveltejs/kit';
 import type { ServerLoad } from '@sveltejs/kit';
+import { arrSyncQueries } from '$db/queries/arrSync.ts';
 
 export const load: ServerLoad = ({ params }) => {
-	// Redirect to the sync tab by default
-	redirect(302, `/arr/${params.id}/sync`);
+	const id = parseInt(params.id || '', 10);
+	const sync = arrSyncQueries.getFullSyncData(id);
+
+	const hasSyncConfig =
+		sync.qualityProfiles.selections.length > 0 ||
+		(!!sync.delayProfiles.databaseId && !!sync.delayProfiles.profileName) ||
+		!!sync.mediaManagement.namingDatabaseId ||
+		!!sync.mediaManagement.qualityDefinitionsDatabaseId ||
+		!!sync.mediaManagement.mediaSettingsDatabaseId;
+
+	redirect(302, `/arr/${params.id}/${hasSyncConfig ? 'library' : 'sync'}`);
 };
